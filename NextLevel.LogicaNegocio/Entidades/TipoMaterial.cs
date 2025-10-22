0bc6b59c4af7d7b1212195800a1112a985e20258
LuciaValverdeCP <lvalverde.lez@gmail.com>
2025-10-22 18:09:20 -0300

    Se incorporaron las entidades con sus respectivos atirbutos

---
+++ NextLevel.LogicaNegocio/Entidades/TipoMaterial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextLevel.LogicaNegocio.Entidades
+{
+    internal class TipoMaterial
+    {
+    }
+}
---
+++ NextLevel.LogicaNegocio/Entidades/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextLevel.LogicaNegocio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Rol {  get; set; }
+
+        public Usuario() { }
+
+        public Usuario (string email, string password, string nombreCompleto)
+        {
+            Email = email;
+            Password = password;
+            NombreCompleto = nombreCompleto;
+        }
+    }
+}
