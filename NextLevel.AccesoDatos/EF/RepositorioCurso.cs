d5d705233c70e46b6101838c6b7134700327a57a
LuciaValverdeCP <lvalverde.lez@gmail.com>
2025-10-24 16:25:53 -0300

    Correcciones

--- NextLevel.AccesoDatos/EF/RepositorioCurso.cs
+++ NextLevel.AccesoDatos/EF/RepositorioCurso.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NextLevel.LogicaNegocio.Entidades;
+using NextLevel.LogicaNegocio.InterfacesRepositorios;
 
 namespace NextLevel.AccesoDatos.EF
 {
-    internal class RepositorioCurso
+    public class RepositorioCurso : IRepositorioCurso
     {
+        private NextLevelContext _db;
+        public RepositorioCurso(NextLevelContext db)
+        {
+            _db = db;
+        }
+        public void Add(Curso obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Curso> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Curso FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Curso obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- NextLevel.AccesoDatos/EF/RepositorioEstudiante.cs
+++ NextLevel.AccesoDatos/EF/RepositorioEstudiante.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NextLevel.LogicaNegocio.Entidades;
+using NextLevel.LogicaNegocio.InterfacesRepositorios;
 
 namespace NextLevel.AccesoDatos.EF
 {
-    internal class RepositorioEstudiante
+    public class RepositorioEstudiante : IRepositorioEstudiante
     {
+        private NextLevelContext _db;
+        public RepositorioEstudiante(NextLevelContext db)
+        {
+            _db = db;
+        }
+        public void Add(Estudiante obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Estudiante> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudiante FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Estudiante obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
