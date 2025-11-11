d6a12a1ade06320d9155675d82e64e0cad5201c5
MauricioUY98 <mauricio8991@gmail.com>
2025-11-11 12:19:28 -0300

    Se realizo la pantalla principal de estudiantes, agregando el boton explorar en el header del mismo. Se modifico el codigo para que en el header no aparezca el enlace de la pantalla que se encuentre activa.

---
+++ WebMVC/Controllers/EstudiantesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NextLevel.LogicaAplicacion.InterfacesCU.Cursos;
+
+namespace WebMVC.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private readonly IObtenerCursos _obtenerCursos;
+        public EstudiantesController(IObtenerCursos obtenerCursos)
+        {
+            _obtenerCursos = obtenerCursos;
+        }
+        public IActionResult Explorar()
+        {
+            if (HttpContext.Session.GetString("rolLogueado") == "Estudiante")
+            {
+                var cursos = _obtenerCursos.Ejecutar();
+                return View("~/Views/Cursos/ListadoCursos.cshtml", cursos);
+            }
+            return RedirectToAction("Login", "Usuarios");
+        }
+        public IActionResult MisCursos()
+        {
+            if(HttpContext.Session.GetString("rolLogueado") == "Estudiante")
+            {
+                return View();
+            }
+            return RedirectToAction("Login", "Usuarios");
+        }
+    }
+}
