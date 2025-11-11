71dec64cf5efbf4922b3df5c21f7ea4d24884dcb
LuciaValverdeCP <lvalverde.lez@gmail.com>
2025-11-11 12:19:36 -0300

    se realizó la pantalla de los cursos en los cuales está inscripto el estudiante

--- NextLevel.LogicaNegocio/InterfacesRepositorios/IRepositorioEstudiante.cs
+++ NextLevel.LogicaNegocio/InterfacesRepositorios/IRepositorioEstudiante.cs
@@ -10,5 +10,6 @@ namespace NextLevel.LogicaNegocio.InterfacesRepositorios
 {
     public interface IRepositorioEstudiante : IRepositorio<Estudiante>
     {
+        Estudiante FindByEmail(string email);
     }
 }
---
+++ WebMVC/Controllers/EstudiantesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NextLevel.LogicaAplicacion.InterfacesCU.Estudiantes;
+
+namespace WebMVC.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private readonly IObtenerMisCursos obtenerMisCursos;
+
+        public EstudiantesController(IObtenerMisCursos obtenerMisCursos)
+        {
+            this.obtenerMisCursos = obtenerMisCursos;
+        }
+
+        public IActionResult MisCursos()
+        {
+            var misCursos = obtenerMisCursos.Ejecutar(HttpContext.Session.GetString("emailLogueado"));
+            return View(misCursos);
+        }
+    }
+}
